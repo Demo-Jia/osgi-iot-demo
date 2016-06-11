32f74ce6b1be608a3420dfc9bf7cd7101195f217
David Bosschaert <bosschae@adobe.com>
2016-06-11 18:43:24 +0100

    Simplify client

--- client/src/main/java/client/BlinkingDeviceClient.java
+++ client/src/main/java/client/BlinkingDeviceClient.java
@@ -1,17 +1,12 @@
 package client;
 
-import java.util.Map;
-
 import org.coderthoughts.iotdemo.BlinkingDevice;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
-@Component(immediate=true,
-    service=BlinkingDeviceClient.class,
-    property={"osgi.command.function=green",
-        "osgi.command.scope=blink"})
+@Component(immediate=true)
 public class BlinkingDeviceClient implements Runnable {
     private volatile boolean keepRunning = true;
 
@@ -26,11 +21,6 @@ public class BlinkingDeviceClient implements Runnable {
     @Deactivate
     public void deactivate() {
         keepRunning = false;
-    }
-
-    public void green() {
-        Map<String, String> res = blinkDevice.blinkGreenLight();
-        System.out.println("Invoked device: " + res);
     }
 
     @Override
